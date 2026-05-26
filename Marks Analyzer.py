dd525fba319fd0c9768c61c7d2971fd5b32acd75
Namratha Chidireddi <namrathachidireddii@gmail.com>
2026-05-26 11:44:51 +0530

    Add files via upload

---
+++ Marks Analyzer.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+marks = np.array([25, 60, 74, 82, 90])
+
+total = np.sum(marks)
+
+average = np.mean(marks)
+
+highest = np.max(marks)
+
+lowest = np.min(marks)
+
+print("Total Marks:", total)
+print("Average Marks:", average)
+print("Highest Marks:", highest)
+print("Lowest Marks:", lowest)
+
+
